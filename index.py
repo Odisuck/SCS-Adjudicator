d080c72b290de0cc437604df15efef8d6c7d9f7c
Odisuck <106165154+Odisuck@users.noreply.github.com>
2022-05-29 15:14:42 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,9 @@
 import asyncio
 from collections import *
-from click import edit
 from discord.ext import commands
 import discord, pickle, string
 from discord_ui import UI, Components, Button, SelectMenu, SelectOption
-import random, time
+import random, time, json, os
 
 DISCORD_TOKEN = ('OTc0MTg4MTI2OTMzMjIxNDI3.Gdtz01.U9DBN77j2-HcXc3_mIzz7mgc71vAlrAg9RVLfE')
 
@@ -14,8 +13,12 @@ quo_list = ['Know thy self, know thy enemy. A thousand battles, a thousand victo
 
 ui = UI(client)
 
-neg_words = {'trash', 'kys', 'tiananmen', '1989', '195394141524328448'}
-pos_words = {'social', 'comrade', 'racism', 'mao', 'communism', 'love china'}
+neg_words = {''}
+pos_words = {''}
+
+
+def positive_serial():
+    pass 
 
 @client.event
 async def on_ready():
@@ -28,8 +31,8 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    global neg_words
-    global pos_words
+    global file_name
+    file_name = f'scs_stat/{message.author}.json'    
 
     pos_count = 0
     neg_count = 0
@@ -39,6 +42,8 @@ async def on_message(message):
 
     msg_trans = message.content.translate(str.maketrans('', '', string.punctuation))
     message_list = msg_trans.lower().split()
+
+    global expo_count
     
     print(message_list)
 
@@ -55,27 +60,24 @@ async def on_message(message):
             pass
 
     if neg_count > pos_count:
-        if neg_count == 1:
-            count = neg_count - pos_count
-            embed=discord.Embed(title="总廷王 dissaproves of your -5 credit score", description=str(random.choice(quo_list)), color=0xff6060)
-            await message.channel.send(embed=embed)
-        else:
-            count = neg_count - pos_count
-            expo_count = round(float(1.9) * pow(count, 2))
-            embed=discord.Embed(title=(f"总廷王 dissaproves of your -{expo_count} credit score"), description=str(random.choice(quo_list)), color=0xff6060)
-            await message.channel.send(embed=embed)
+        count = neg_count - pos_count
+        expo_count = round(float(1.9) * pow(count, 2))
+        embed=discord.Embed(title=(f"总廷王 dissaproves of your -{expo_count} credit score"), description=str(random.choice(quo_list)), color=0xff6060)
+        embed.set_footer()
+        await message.channel.send(embed=embed)
+        negative_serial()
+        await message.channel.send(current_scs())
+
     elif neg_count == pos_count:
         pass
+
     else:
-        if pos_count == 1:
-            count = pos_count - neg_count
-            embed=discord.Embed(title="总廷王 approves of your +5 credit score", description=str(random.choice(quo_list)), color=0x99ff93)
-            await message.channel.send(embed=embed)
-        else:
-            count = int(pos_count - neg_count)
-            expo_count = round(5 * count * float(1.25))
-            embed=discord.Embed(title=(f"总廷王 approves of your +{expo_count} credit score"), description=str(random.choice(quo_list)), color=0x99ff93)
-            await message.channel.send(embed=embed)
+        count = int(pos_count - neg_count)
+        expo_count = round(5 * count * float(1.25))
+        embed=discord.Embed(title=(f"总廷王 approves of your +{expo_count} credit score"), description=str(random.choice(quo_list)), color=0x99ff93)
+        await message.channel.send(embed=embed)
+        positive_serial()
+        await message.channel.send(current_scs())
 
     #suggest 'command'
     if message.content.startswith('gest'):
@@ -86,6 +88,7 @@ async def on_message(message):
 
         if message_form == 2:
 
+            global s_word
             s_word = message_list[1]
             
             if s_word != 'gest':
@@ -141,8 +144,66 @@ async def on_message(message):
 
     await client.process_commands(message)
 
+def negative_serial():
+    if os.path.isfile(file_name):
+        print('found file')
+
+        json_file = open(file_name)
+        dict = json.load(json_file)
+        json_file.close()
+
+        pre_score = dict["score"]
+        new_score = int(pre_score) - int(expo_count)
+
+        def_dict = {'score': new_score}
+
+        with open(file_name, "w") as f:
+            json.dump(def_dict, f)
+
+    else:
+        def_dict = {'score': -abs(expo_count)}
+
+        with open(file_name, "w") as f:
+            json.dump(def_dict, f)
+    
+def positive_serial():
+    if os.path.isfile(file_name):
+        print('found file')
+
+        json_file = open(file_name)
+        dict = json.load(json_file)
+        json_file.close()
+
+        pre_score = dict["score"]
+        new_score = int(pre_score) + int(expo_count)
+
+        def_dict = {'score': new_score}
+
+        with open(file_name, "w") as f:
+            json.dump(def_dict, f)
+
+    else:
+        def_dict = {'score': expo_count}
+
+        with open(file_name, "w") as f:
+            json.dump(def_dict, f)
+
+def current_scs():
+    json_file = open(file_name)
+    dict = json.load(json_file)
+    json_file.close()
+
+    current_score = dict["score"]
+    return current_score
+
+
+
+def add_pos():
+    pass
+
+
 @client.listen('on_button')
 async def on_button(btn):
     pass
 
-client.run(DISCORD_TOKEN)
+client.run(DISCORD_TOKEN)   
